6c918aedd59fd63704f3f2be88ef3e32c45bf097
Hamzah Mahmood <hmzmh2020@gmail.com>
2023-07-13 07:52:29 +0100

    Improved styling

--- src/components/AboutMeContent.js
+++ src/components/AboutMeContent.js
@@ -9,7 +9,18 @@ function AboutMeContent() {
 
             <img src={MeEdited} alt='MeEdited' className='Me-Edited'/>
 
-            <h3>I enjoy writing clean code and creating stylish sites.</h3>
+            <h3 className='About-H3'>My aim is to provide end-users with a delightfully impactful yet seamlessly comfortable experience.</h3>
+            
+            <p1 className='About-P1'>
+                As a frontend developer, I follow the philosophy that form follows function.
+                <br/>
+                I take pride in crafting intuitive interfaces, writing clean & maintainable code, for user-centric software.
+                <br/>
+                In crafting interfaces, I value usability, structure, simplicity, consistency, a smart use of space & a careful use of colour.
+                <br/>
+                In writing code, I prioritise readability, robustness & rigour, reliability & dependability, reusability, low coupling & high cohesion.
+                <br/>
+            </p1>
 
         </div>
 
--- src/components/ProjectsContent.js
+++ src/components/ProjectsContent.js
@@ -5,12 +5,15 @@ return(
 <div id='Content-3' className='Projects-Content'>
     
     <div className='Project-Tiles'>
+        <h3 className='Tiles-H3'>Interface Design</h3>
     </div>
 
     <div className='Project-Tiles'>
+        <h3 className='Tiles-H3'>System Design</h3>
     </div>
 
     <div className='Project-Tiles'>
+    <h3 className='Tiles-H3'>Mobile Application</h3>
     </div>
 
 </div>
