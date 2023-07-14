20d65f318906a62a8caed7636421be45a9d90fac
Hamzah Mahmood <hmzmh2020@gmail.com>
2023-07-14 09:01:53 +0100

    Site redesign

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import SocialLinks from './components/SocialsLinks.js';
-import NavigationPanel from './components/NavigationPanel';
-import AboutMeContent from './components/AboutMeContent';
-import EducationContent from './components/EducationContent';
-import ProjectsContent from './components/ProjectsContent';
-import WaveVector from './images/Wave_Vector.png';
 
 function App() {
   return (
     <div className='Page'>
-
-      <NavigationPanel/>
-
+      
       <SocialLinks/>
-
-      <div className='Header-1'>
-          <h1>Hamzah Mahmood</h1>
-      </div>
-      
-      <div className='Header-2'>
-          <h2>Software Engineer | Frontend Developer</h2>
-      </div>
-
-      <AboutMeContent/>
-
-      <EducationContent/>
-      <br/>
-
-      <img src={WaveVector} alt='WaveVector' className='Wave-Vector'/>
-      
-      <ProjectsContent/>
-      <br/>
 
     </div>
   );
