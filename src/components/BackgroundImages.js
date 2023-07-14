354b47fd829479d8d72ff415ff384c2afb88ff74
Hamzah Mahmood <hmzmh2020@gmail.com>
2023-07-14 11:17:33 +0100

    Added background video

--- src/components/BackgroundImages.js
+++ src/components/BackgroundImages.js
@@ -1,4 +1,6 @@
+import '../styles/BackgroundImages.css'
 import MyName from '../images/My_Name.png';
+import WavesVideo from '../images/Waves_Video.mp4';
 
 function BackgroundImages(){
     
@@ -6,10 +8,19 @@ function BackgroundImages(){
 
       <div class="d-flex justify-content-center">
         
-        <img src={MyName} alt='My signature' class="img-fluid"/>
-      
-      </div>
+        <video className='Background-Video' autoPlay muted loop>
 
+             <source src={WavesVideo} alt="Waves" type="video/mp4"/>
+
+        </video>
+
+        <div>
+            
+            <img src={MyName} alt="My name" class="img-fluid" style={{zIndex:'1'}}/>
+        
+        </div>
+
+      </div>
     );
 
 }
