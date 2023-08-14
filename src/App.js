308ff865533a31c1e2fe427edeb4a12b0ef4f8b0
Hamzah Mahmood <hmzmh2020@gmail.com>
2023-08-14 16:52:34 +0100

    Added about section

--- src/App.js
+++ src/App.js
@@ -23,19 +23,38 @@ function Home() {
 
         <div id='content-component' className="row justify-content-center">
 
-          <div id='photo-box'>
-            <img id='portrait-photo' src={Portrait_Photo} alt='Portrait'/>
-          </div>
-
           <h1 style={{fontSize: "75px", textAlign: "left", marginInlineStart:'22vw', marginTop: "20vh"}}>Hi. I'm Hamzah. A software engineer.</h1>
           
-          <Tile1/>
-          <Tile2/>
-          <Tile3/>
-          <Tile4/>
-        </div>
+          <h1 className='h1-sections'>About me</h1>
+          <div id='about-section' className="row justify-content-center">
+            <div id='photo-box'>
+              <img id='portrait-photo' src={Portrait_Photo} alt='Portrait'/>
+            </div>
 
-        <div></div>
+            <div id='about-paragraph'>
+              <p>
+                     I'm a fresh software engineer graduate with a passion for frontend development.
+                <br/>I take pride in creating software that is very simple but very attractive and very effective.
+                <br/>I enjoy the challenge of turning complex problems into elegant, user-centric software.
+                <br/>
+                <br/>Currently, I'm on the lookout for opportunities that allow me to learn new skills and expand my horizons.
+                <br/>I'm keen to contribute to a dynamic team where innovative thinking and problem-solving are celebrated.
+              </p>
+            </div>
+
+          </div>
+
+          <h1 className='h1-sections'>My projects</h1>
+          <div id='project-section' className="row justify-content-center">
+            <Tile1/>
+            <Tile2/>
+            <Tile3/>
+            <Tile4/>
+          </div>
+
+          <div id='footer' className="row justify-content-center"></div>
+
+        </div>
         
         <div id='fixed-component' className="row justify-content-start"><AccessButtons/></div>
         
