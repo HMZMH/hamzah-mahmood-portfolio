62bf66e81602dacca8a04aaee6c04dd7a32d8b6e
Hamzah Mahmood <hmzmh2020@gmail.com>
2023-08-15 21:23:24 +0100

    Added spotify player

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ import HTML_Logo from './images/Html_Logo.png';
 import Java_Logo from './images/Java_Logo.png';
 import Javascript_Logo from './images/Javascript_Logo.png';
 import React_Logo from './images/React_Logo.png';
+import football from './images/Football.png';
+import united from './images/United.png';
+import gaming from './images/Gaming.png';
+import cardio from './images/Cardio.png';
+import headphones from './images/Headphones.png';
 import Tile1 from './components/Tile1.js';
 import Tile2 from './components/Tile2.js';
 import Tile3 from './components/Tile3.js';
@@ -34,18 +39,42 @@ function Home() {
             <h1 className='h1-sections'>About me</h1>
 
             <p>
-                I'm a fresh software engineer graduate with a passion for frontend development.
+                I'm a recent software engineer graduate with a strong interest in frontend development.
               <br/>I take pride in creating software that is very simple but equally attractive and effective.
               <br/>I enjoy the challenge of turning complex problems into elegant, user-centric software.
               <br/>I'm looking for opportunities that allow me to learn new skills and expand my horizons.
             </p>
 
-            <div className='row justify-content-end'>
+            <div className='row justify-content-end' style={{marginTop:'-16vh'}}>
               <img src={CSS_Logo} alt='CSS logo' className='skill-logo'/>
               <img src={HTML_Logo} alt='HTML logo' className='skill-logo'/>
               <img src={Javascript_Logo} alt='Javascript logo' className='skill-logo'/>
               <img src={Java_Logo} alt='Java logo' className='skill-logo'/>
               <img src={React_Logo} alt='React logo' className='skill-logo'/>
+            </div>
+
+            <p>
+              <br/>Away from work, I love football: playing 5-a-side, watching Man United and playing FIFA.
+              <br/>I often enjoy PS5 multiplayer games with friends, to satisfy my need for competitive fun.
+              <br/>I also go for long walks and hit the gym regularly, as to ensure I remain fit and healthy.
+              <br/>And to keep up to date with tech news, I spend a bit time watching channels like MKBHD.
+            </p>
+            
+            <div className='row justify-content-end' style={{marginTop:'-13vh'}}>
+              <img src={gaming} alt='Gaming icon' className='skill-logo'/>
+              <img src={headphones} alt='Headphones icon' className='skill-logo'/>
+              <img src={cardio} alt='Cardio icon' className='skill-logo'/>
+              <img src={football} alt='Football icon' className='skill-logo'/>
+              <img src={united} alt='Man United logo' className='skill-logo'/>
+            </div>
+
+            <p>
+              <br/>
+              <br/>Also, whether I'm working or playing, I like to have a few songs playing in the background:
+            </p>
+
+            <div className='row justify-content-end' style={{marginTop:'-5vh'}}>
+              <iframe id='spotify-player' src="https://open.spotify.com/embed/playlist/1Y9gUnk8OPtkAlDX4qKb5Y?utm_source=generator&theme=0" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
             </div>
 
           </div>
--- src/components/Project2.js
+++ src/components/Project2.js
@@ -4,6 +4,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/Project2.css';
 import SocialsButtons from '../components/SocialsButtons.js';
 import AccessButtons from '../components/AccessButtons.js';
+import project2image1 from '../images/Project2-Image1.png';
+import project2image2 from '../images/Project2-Image2.png';
+import project2image3 from '../images/Project2-Image3.png';
+import project2image4 from '../images/Project2-Image4.png';
+import project2image5 from '../images/Project2-Image5.png';
 
 function Project2() {
   return (
@@ -11,10 +16,33 @@ function Project2() {
 
         <div id='project2-fixed-component' className="row justify-content-start"><SocialsButtons/></div>
 
-        <div id='project2-content-component' className="row justify-content-center">
+        <div id='project2-content-component' className="row justify-content-start">
 
-        <nav><Link id='link' to="/"><h1>&lt; Interface Design Project</h1></Link></nav>
-        
+          <nav>
+            <Link id='link' to="/">
+              <h3>&lt; Home</h3>
+            </Link>
+          </nav>
+
+          <h1 className='h1-sections'>System Design Project</h1>
+
+          <p>This project entailed designing a web-application at system level,
+          <br/>to meet requirements identified through analysing a provided problem statement.<br/> 
+          <br/> 
+          The system was designed through UML diagrams:<br/> 
+          -Use Case diagrams<br/> 
+          -Class diagrams<br/> 
+          -Sequence diagrams<br/> 
+          </p>
+          
+          <div className="row justify-content-end">
+          <img id='project2-image1' alt='Project2 image1' src={project2image1}/>      
+          <img id='project2-image2' alt='Project2 image2' src={project2image2}/>    
+          <img id='project2-image3' alt='Project2 image3' src={project2image3}/>    
+          <img id='project2-image4' alt='Project2 image4' src={project2image4}/>
+          <img id='project2-image5' alt='Project2 image5' src={project2image5}/>
+          </div>
+      
         </div>
 
         <div id='project2-fixed-component' className="row justify-content-start"><AccessButtons/></div>
