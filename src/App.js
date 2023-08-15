4553bcf029ec71b970a8878794e2425aba3995fa
Hamzah Mahmood <hmzmh2020@gmail.com>
2023-08-15 23:34:14 +0100

    Improved 'about' section styling

--- src/App.js
+++ src/App.js
@@ -38,43 +38,54 @@ function Home() {
 
             <h1 className='h1-sections'>About me</h1>
 
-            <p>
-                I'm a recent software engineer graduate with a strong interest in frontend development.
-              <br/>I take pride in creating software that is very simple but equally attractive and effective.
-              <br/>I enjoy the challenge of turning complex problems into elegant, user-centric software.
-              <br/>I'm looking for opportunities that allow me to learn new skills and expand my horizons.
-            </p>
+            <div id='about-divs' className='row justify-content-end'>
 
-            <div className='row justify-content-end' style={{marginTop:'-16vh'}}>
-              <img src={CSS_Logo} alt='CSS logo' className='skill-logo'/>
-              <img src={HTML_Logo} alt='HTML logo' className='skill-logo'/>
-              <img src={Javascript_Logo} alt='Javascript logo' className='skill-logo'/>
-              <img src={Java_Logo} alt='Java logo' className='skill-logo'/>
-              <img src={React_Logo} alt='React logo' className='skill-logo'/>
+              <p>
+                <br/>I'm a recent software engineer graduate with a strong interest in frontend development.
+                <br/>I take pride in creating software that is very simple but equally attractive and effective.
+                <br/>I enjoy the challenge of turning complex problems into elegant, user-centric software.
+                <br/>I'm looking for opportunities that allow me to learn new skills and expand my horizons.
+              </p>
+
+              <div className='skill-div'>
+                <img src={CSS_Logo} alt='CSS logo' className='skill-logo'/>
+                <img src={HTML_Logo} alt='HTML logo' className='skill-logo'/>
+                <img src={Javascript_Logo} alt='Javascript logo' className='skill-logo'/>
+                <img src={Java_Logo} alt='Java logo' className='skill-logo'/>
+                <img src={React_Logo} alt='React logo' className='skill-logo'/>
+              </div>
+
             </div>
-
-            <p>
-              <br/>Away from work, I love football: playing 5-a-side, watching Man United and playing FIFA.
-              <br/>I often enjoy PS5 multiplayer games with friends, to satisfy my need for competitive fun.
-              <br/>I also go for long walks and hit the gym regularly, as to ensure I remain fit and healthy.
-              <br/>And to keep up to date with tech news, I spend a bit time watching channels like MKBHD.
-            </p>
             
-            <div className='row justify-content-end' style={{marginTop:'-13vh'}}>
-              <img src={gaming} alt='Gaming icon' className='skill-logo'/>
-              <img src={headphones} alt='Headphones icon' className='skill-logo'/>
-              <img src={cardio} alt='Cardio icon' className='skill-logo'/>
-              <img src={football} alt='Football icon' className='skill-logo'/>
-              <img src={united} alt='Man United logo' className='skill-logo'/>
+            <div id='about-divs' className='row justify-content-end'>
+
+              <p>
+                <br/>Away from work, I love football: playing 5-a-side, watching Man United and playing FIFA.
+                <br/>I often enjoy PS5 multiplayer games with friends, to satisfy my need for competitive fun.
+                <br/>I also go for long walks and hit the gym regularly, as to ensure I remain fit and healthy.
+                <br/>And to keep up to date with tech news, I spend a bit time watching channels like MKBHD.
+              </p>
+
+              <div className='skill-div'>
+                <img src={united} alt='Man United logo' className='skill-logo'/>
+                <img src={football} alt='Football icon' className='skill-logo'/>
+                <img src={gaming} alt='Gaming icon' className='skill-logo'/>
+                <img src={cardio} alt='Cardio icon' className='skill-logo'/>
+                <img src={headphones} alt='Headphones icon' className='skill-logo'/>
+              </div>
+      
             </div>
 
-            <p>
-              <br/>
-              <br/>Also, whether I'm working or playing, I like to have a few songs playing in the background:
-            </p>
+            <div id='about-divs' className='row justify-content-end'>
 
-            <div className='row justify-content-end' style={{marginTop:'-5vh'}}>
-              <iframe id='spotify-player' src="https://open.spotify.com/embed/playlist/1Y9gUnk8OPtkAlDX4qKb5Y?utm_source=generator&theme=0" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+              <p>
+                <br/>Also, whether I'm working or playing, I like to have a few songs playing in the background:
+              </p>
+
+              <div className='skill-div'>
+                <iframe id='spotify-player' src="https://open.spotify.com/embed/playlist/1Y9gUnk8OPtkAlDX4qKb5Y?utm_source=generator&theme=0" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+              </div>
+
             </div>
 
           </div>
